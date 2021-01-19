bc85fa47a3cdc6650a4bd826d5f7103991986a91
majbah08 <majbahbuet08@gmail.com>
2021-01-19 14:17:55 +0600

    changed post req and print

--- src/main/java/com/majbah/dsidemo/WelcomeController.java
+++ src/main/java/com/majbah/dsidemo/WelcomeController.java
@@ -1,24 +1,30 @@
 package com.majbah.dsidemo;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
+
 @RestController
 public class WelcomeController {
+
+    Logger log = Logger.getLogger(WelcomeController.class.getName());
 
     @GetMapping("/")
     public String  get(){
 
         return "Hello";
     }
-    @PostMapping(value = "/", consumes ="application/json" ,produces ="application/json" )
-    public ResponseEntity<String>  post(@RequestBody String req){
-
-        return ResponseEntity.ok(req);
+    @PostMapping(value = "/" )
+    public ResponseEntity post(@RequestBody String req){
+        log.info(req);
+        return (ResponseEntity) ResponseEntity.status(HttpStatus.OK);
     }
 
 }
